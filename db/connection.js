52e3d1bb8b67e28dbc7769819d1f3fea4cbaa55c
aleksandre <aleksandre.chkhikvishvili1@gmail.com>
2024-03-13 09:48:52 -0400

    Aleksandre

--- db/connection.js
+++ db/connection.js
@@ -3,6 +3,8 @@ import chalk from "chalk";
 
 mongoose.set("returnOriginal", false);
 
+const connectionString = process.env.DB_URL || "mongodb://127.0.0.1:27017/countries_api"
+
 mongoose.connect("mongodb://127.0.0.1:27017/countries_api").catch((err) => {
   console.log(`Error connection to MongoDB: ${err.message}`);
 });
