9423464d35c8a62e793dc47e6b97dea604f06d19
aleksandre <aleksandre.chkhikvishvili1@gmail.com>
2024-03-13 10:11:33 -0400

    deploy ready

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ mongoose.set("returnOriginal", false);
 
 const connectionString = process.env.DB_URL || "mongodb://127.0.0.1:27017/countries_api"
 
-mongoose.connect("mongodb://127.0.0.1:27017/countries_api").catch((err) => {
+mongoose.connect(connectionString).catch((err) => {
   console.log(`Error connection to MongoDB: ${err.message}`);
 });
 
